495d3090153a1851d5565ab7bd28e0b1eaedc13e
ma <ma_yamaki@yahoo.com>
2021-02-12 15:20:58 +0900

    dd

---
+++ public/scripts/main2.js
@@ -0,0 +1,14 @@
+
+let vm = new Vue({
+    el: '#app',
+    data: {
+        message: "Hello Vue",
+        newNum:"",
+        nums: []
+    },
+    methods:{
+      addItem:function(event){
+        alert();
+      }
+    }
+})
